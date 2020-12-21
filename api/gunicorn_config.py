fbe13728e2b91b49bbe423c3ce51604eddde4dee
Milan Gupta <milagu@umich.edu>
2020-12-21 11:11:12 -0500

    changed permissions on sock

--- api/gunicorn_config.py
+++ api/gunicorn_config.py
@@ -1,8 +1,8 @@
 import multiprocessing
 
 workers = multiprocessing.cpu_count() * 2 + 1
-bind = 'unix:/var/www/flaskapi.sock'
-umask = 0o007
+bind = 'unix:/var/run/flaskapi.sock'
+umask = 0o000
 reload = True
 
 accesslog = '-'

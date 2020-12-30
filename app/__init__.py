2d4edc9e393f01316dc8014fa6e593dddc3d5baa
Milan Gupta <milagu@umich.edu>
2020-12-30 15:54:51 -0500

    try this

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 import pymongo
 from flask import Flask, g
 
+from app.main import main
 from app.api.auth import auth
 from app.config import config
 
 application = Flask(__name__)
 application.config.update(config)
 
+application.register_blueprint(main)
 application.register_blueprint(auth, url_prefix='/api/auth')
 
 client = pymongo.MongoClient(application.config['MONGO_CONNECTION'])
--- app/main.py
+++ app/main.py
@@ -6,6 +6,8 @@ from flask import Response, g
 from app.flaskrun import flaskrun
 from app import application
 
+main = Blueprint('main', __name__)
+
 @application.route('/', methods=['GET'])
 def get():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)

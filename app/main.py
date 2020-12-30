ef2fa555ea6affadeffdd97c7082085e024e1c9a
Milan Gupta <milagu@umich.edu>
2020-12-30 15:55:38 -0500

    ok this time

--- app/main.py
+++ app/main.py
@@ -4,11 +4,10 @@ import json
 from flask import Response, g
 
 from app.flaskrun import flaskrun
-from app import application
 
 main = Blueprint('main', __name__)
 
-@application.route('/', methods=['GET'])
+@main.route('/', methods=['GET'])
 def get():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
 
